603d5dc04322a3e6c4e3126c7da7d0eefcdcd210
edaywalid <becharwalid9@gmail.com>
2024-10-19 20:15:49 +0100

    updated the missing ingerdients details when fetching

--- internal/repositories/product_repository.go
+++ internal/repositories/product_repository.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"context"
 
+	"github.com/edaywalid/pinktober-hackathon-backend/internal/dto"
 	"github.com/edaywalid/pinktober-hackathon-backend/internal/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -10,29 +11,36 @@ import (
 )
 
 type ProductRepository struct {
-	collection *mongo.Collection
+	product_collection    *mongo.Collection
+	ingredient_collection *mongo.Collection
 }
 
-func NewProductRepository(collection *mongo.Collection) *ProductRepository {
-	return &ProductRepository{collection: collection}
+func NewProductRepository(
+	product_collection *mongo.Collection,
+	ingredient_collection *mongo.Collection,
+) *ProductRepository {
+	return &ProductRepository{
+		product_collection:    product_collection,
+		ingredient_collection: ingredient_collection,
+	}
 }
 
 func (r *ProductRepository) CreateProduct(ctx context.Context, product *models.Product) error {
-	_, err := r.collection.InsertOne(ctx, product)
+	_, err := r.product_collection.InsertOne(ctx, product)
 	return err
 }
 
-func (r *ProductRepository) GetProducts(ctx context.Context, page, perPage int) ([]models.Product, int64, error) {
+func (r *ProductRepository) GetProducts(ctx context.Context, page, perPage int) ([]dto.ProductDTO, int64, error) {
 	skip := int64((page - 1) * perPage)
 	limit := int64(perPage)
 
-	totalCount, err := r.collection.CountDocuments(ctx, bson.M{})
+	totalCount, err := r.product_collection.CountDocuments(ctx, bson.M{})
 	if err != nil {
 		return nil, 0, err
 	}
 
 	findOptions := options.Find().SetSkip(skip).SetLimit(limit)
-	cursor, err := r.collection.Find(ctx, bson.M{}, findOptions)
+	cursor, err := r.product_collection.Find(ctx, bson.M{}, findOptions)
 	if err != nil {
 		return nil, 0, err
 	}
@@ -43,11 +51,57 @@ func (r *ProductRepository) GetProducts(ctx context.Context, page, perPage int) 
 		return nil, 0, err
 	}
 
-	return products, totalCount, nil
+	var productsDTO []dto.ProductDTO
+
+	for _, product := range products {
+		var ingredients []models.Ingredient
+
+		filter := bson.M{"_id": bson.M{"$in": product.Ingredients}}
+		ingredientCursor, err := r.ingredient_collection.Find(ctx, filter)
+		if err != nil {
+			return nil, 0, err
+		}
+		if err = ingredientCursor.All(ctx, &ingredients); err != nil {
+			return nil, 0, err
+		}
+
+		productDTO := dto.ProductDTO{
+			Barcode:     product.Barcode,
+			Name:        product.Name,
+			Brand:       product.Brand,
+			Ingredients: ingredients, OverallRisk: product.OverallRisk,
+		}
+
+		productsDTO = append(productsDTO, productDTO)
+	}
+
+	return productsDTO, totalCount, nil
 }
 
-func (r *ProductRepository) GetProductByBarcode(ctx context.Context, barcode string) (models.Product, error) {
+func (r *ProductRepository) GetProductByBarcode(ctx context.Context, barcode string) (dto.ProductDTO, error) {
 	var product models.Product
-	err := r.collection.FindOne(ctx, bson.M{"_id": barcode}).Decode(&product)
-	return product, err
+	err := r.product_collection.FindOne(ctx, bson.M{"_id": barcode}).Decode(&product)
+	if err != nil {
+		return dto.ProductDTO{}, err
+	}
+
+	var ingredients []models.Ingredient
+	filter := bson.M{"_id": bson.M{"$in": product.Ingredients}}
+	ingredientCursor, err := r.ingredient_collection.Find(ctx, filter)
+	if err != nil {
+		return dto.ProductDTO{}, err
+	}
+	if err = ingredientCursor.All(ctx, &ingredients); err != nil {
+		return dto.ProductDTO{}, err
+	}
+
+	productDTO := dto.ProductDTO{
+		Barcode:     product.Barcode,
+		Name:        product.Name,
+		Brand:       product.Brand,
+		Ingredients: ingredients,
+		OverallRisk: product.OverallRisk,
+	}
+
+	return productDTO, nil
 }
