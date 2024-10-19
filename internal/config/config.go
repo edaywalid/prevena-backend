6b0fdc609802899e0247d7adfa5874af9d9bd2c5
edaywalid <becharwalid9@gmail.com>
2024-10-19 10:02:14 +0100

    update the config handling to seperate prod and dev

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,38 +1,35 @@
 package config
 
-import "github.com/joho/godotenv"
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 type Config struct {
 	ENV  string
 	PORT string
 }
 
+func getEnv(key string, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
 func LoadConfig() (*Config, error) {
 	var config Config
-	if err := godotenv.Load(); err != nil {
-		return nil, err
+
+	if getEnv("ENV", "development") == "development" {
+		if err := godotenv.Load(); err != nil {
+			return nil, fmt.Errorf("Error loading .env file: %v", err)
+		}
 	}
 
 	config.ENV = getEnv("ENV", "development")
 	config.PORT = getEnv("PORT", "8080")
 
 	return &config, nil
-}
-
-func getEnv(key string, defaultValue string) string {
-	value, exists := LookupEnv(key)
-	if exists {
-		return value
-	}
-
-	return defaultValue
-}
-
-func LookupEnv(key string) (string, bool) {
-	if env_map, err := godotenv.Read(); err == nil {
-		if value, exists := env_map[key]; exists {
-			return value, true
-		}
-	}
-	return "", false
 }
