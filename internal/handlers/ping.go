8db970ea143f84728eb209cc1bee47b7644d887d
edaywalid <becharwalid9@gmail.com>
2024-10-19 12:17:08 +0100

    added a pingHandler type for better management

--- internal/handlers/ping.go
+++ internal/handlers/ping.go
@@ -4,6 +4,10 @@ import "net/http"
 
 type PingHandler struct{}
 
+func NewPingHandler() *PingHandler {
+	return &PingHandler{}
+}
+
 func (ph *PingHandler) Ping(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("pong"))
