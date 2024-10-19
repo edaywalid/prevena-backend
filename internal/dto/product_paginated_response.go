a157bbcb0bd611933920430ba4877c90bb7fb97f
edaywalid <becharwalid9@gmail.com>
2024-10-19 20:15:36 +0100

    returning ProductDTO in the pagination response

--- internal/dto/product_paginated_response.go
+++ internal/dto/product_paginated_response.go
@@ -1,11 +1,9 @@
 package dto
 
-import "github.com/edaywalid/pinktober-hackathon-backend/internal/models"
-
 type PaginatedResponse struct {
-	Products   []models.Product `json:"products"`
-	TotalCount int64            `json:"total_count"`
-	Page       int              `json:"page"`
-	PerPage    int              `json:"per_page"`
-	TotalPages int              `json:"total_pages"`
+	Products   []ProductDTO `json:"products"`
+	TotalCount int64        `json:"total_count"`
+	Page       int          `json:"page"`
+	PerPage    int          `json:"per_page"`
+	TotalPages int          `json:"total_pages"`
 }
