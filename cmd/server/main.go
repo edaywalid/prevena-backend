c3ea8c27aaece03315f9df19fb121192ac7ed317
edaywalid <becharwalid9@gmail.com>
2024-10-19 09:39:41 +0100

    set up the main server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
-	"os"
+	"net/http"
 
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
+	"github.com/edaywalid/pinktober-hackathon-backend/internal/di"
+	"github.com/edaywalid/pinktober-hackathon-backend/internal/router"
+	"github.com/edaywalid/pinktober-hackathon-backend/pkg/logger"
 )
 
 func main() {
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-	log.Info().Msg("Starting the application")
+	log := logger.NewLogger()
+
+	container, err := di.NewContainer()
+	if err != nil {
+		log.LogError().Msgf("Error creating container: %v", err)
+		return
+	}
+	log.LogInfo().Msg("Container created successfully")
+
+	log.LogInfo().Msg("Setting up router")
+	router := router.SetupRouter(container)
+	log.LogInfo().Msg("Router setup successfully")
+
+	log.LogInfo().Msg("Starting server")
+	log.LogInfo().Msgf("Server started on port %s", container.Config.PORT)
+
+	if err := http.ListenAndServe(":"+container.Config.PORT, router); err != nil {
+		log.LogError().Msgf("Error starting server: %v", err)
+	}
+
 }
