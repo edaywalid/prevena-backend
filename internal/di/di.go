b2b1ad7c793cd2751d489d9aacd338e9e4099046
edaywalid <becharwalid9@gmail.com>
2024-10-19 12:07:17 +0100

    seperated the concerns and added loggings

--- internal/di/di.go
+++ internal/di/di.go
@@ -3,8 +3,10 @@ package di
 import (
 	"context"
 	"time"
+
 	"github.com/edaywalid/pinktober-hackathon-backend/internal/config"
 	"github.com/edaywalid/pinktober-hackathon-backend/internal/handlers"
+	"github.com/edaywalid/pinktober-hackathon-backend/pkg/logger"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -15,6 +17,7 @@ type Container struct {
 	Handlers     *Handlers
 	Config       *config.Config
 	Databases    *Databases
+	Logger       *logger.MyLogger
 }
 
 type (
@@ -28,20 +31,33 @@ type (
 	}
 )
 
-func NewContainer() (*Container, error) {
+func NewContainer(log *logger.MyLogger) (*Container, error) {
 	var container Container
+	container.Logger = log
+
+	container.Logger.LogInfo().Msg("Loading config")
 	config, err := config.LoadConfig()
 	if err != nil {
+		container.Logger.LogError().Msgf("Error loading config: %v", err)
 		return nil, err
 	}
+	container.Logger.LogInfo().Msg("Config loaded successfully")
 	container.Config = config
-	container.Services = &Services{}
-	container.Repositories = &Repositories{}
-	container.Handlers = &Handlers{
-		PingHandler: &handlers.PingHandler{},
+
+	container.Logger.LogInfo().Msg("Initializing databases")
 	if err := container.initDatabases(); err != nil {
+		log.LogError().Msgf("Error initializing databases: %v", err)
 		return nil, err
 	}
+	log.LogInfo().Msg("Databases initialized successfully")
+
+	log.LogInfo().Msg("Initializing services, repositories, and handlers")
+	container.InitServices()
+	log.LogInfo().Msg("Services initialized successfully")
+	container.InitRepositories()
+	log.LogInfo().Msg("Repositories initialized successfully")
+	container.InitHandlers()
+	log.LogInfo().Msg("Handlers initialized successfully")
 
 	return &container, nil
 }
@@ -64,7 +80,26 @@ func (c *Container) initDatabases() error {
 	return nil
 }
 
+func (c *Container) InitServices() {
+	services := &Services{}
+	c.Services = services
+
+}
+
+func (c *Container) InitRepositories() {
+	repositories := &Repositories{}
+	c.Repositories = repositories
+}
+
+func (c *Container) InitHandlers() {
+	handlers := &Handlers{
+		PingHandler: handlers.NewPingHandler(),
+	}
+	c.Handlers = handlers
+}
+
 func (c *Container) Close() error {
+	c.Logger.LogInfo().Msg("Closing databases")
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	if c.Databases.DB != nil {
